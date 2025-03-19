ecaeda0db46abd00cb29ea855e6bc516ab644071
Tulxy <tomas.vylimec@gmail.com>
2025-03-19 17:51:49 +0100

    commit max_price function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ products = [
 
 def print_products():
     for product in products:
-        print(f"Název produktu2: {product['name']}, cena: {product['price']}$")
+        print(f"Název produktu: {product['name']}, cena: {product['price']}$")
 
 
 def add_product():
@@ -31,22 +31,34 @@ def total_price():
     print(f"Celková cena: {total}$")
 
 
+def max_price():
+    maximum_price = max([product['price'] for product in products])
+    print(f"Nejvyšší cena: {maximum_price}$")
+
+
 def menu():
     print("Vítej ve skladu")
     print("###############\n")
     print("1. Výpis polože")
     print("2. Přidání položky")
     print("3. Celková cena")
+    print("4. Nejvyšší cena")
 
     choice = int(input("Volba: "))
 
     match choice:
         case 1:
+            print("Vypsání položek")
             print_products()
         case 2:
+            print("Přidání položky")
             add_product()
         case 3:
+            print("Celková cena")
             total_price()
+        case 4:
+            print("Nejvyšší cena")
+            max_price()
         case _:
             print("Neplatná volba")
 
