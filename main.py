d21acf73145974a8b2fb66e08e31fcea0b963774
Tulxy <tomas.vylimec@gmail.com>
2025-03-23 18:52:08 +0100

    commit fixed bugs

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def add_product():
 
 
 def remove_product():
-    remove_all = int(input("Chete odtranit všechny produkty? 1 - ano, 2 - ne: "))
+    remove_all = int(input("Chcete odstranit všechny produkty? 1 - ano, 2 - ne: "))
     if remove_all == 1:
         products.clear()
         print_products()
@@ -57,33 +57,34 @@ def remove_product():
 
 
 def search_product():
-  available = input("Chceš zobrazit pouze produkty, které jsou skladem? 1 - ano, 2 - ne: ")
-  search = input("Zadej název produktu: ")
-  found = False
+    available = input("Chceš zobrazit pouze produkty, které jsou skladem? 1 - ano, 2 - ne: ")
+    search = input("Zadej název produktu: ")
+    found = False
 
-  for product in products:
-    name_match = search.lower() in product['name'].lower()
-    in_stock = product['availability'] == "Skladem"
+    for product in products:
+        name_match = search.lower() in product['name'].lower()
+        in_stock = product['availability'] == "Skladem"
 
-    if available == "1":
-      if name_match and in_stock:
-        print(f"Název produktu: {product['name']}\n cena: {product['price']}$\n množství: {product['amount']}")
-        found = True
-    else:
-      if name_match:
-        print(
-          f"Název produktu: {product['name']}\n cena: {product['price']}$\n množství: {product['amount']}\n {product['availability']}")
-        found = True
+        if available == "1":
+            if name_match and in_stock:
+                print(f"Název produktu: {product['name']}\n cena: {product['price']}$\n množství: {product['amount']}")
+                found = True
+        else:
+            if name_match:
+                print(
+                    f"Název produktu: {product['name']}\n cena: {product['price']}$\n množství: {product['amount']}\n {product['availability']}")
+                found = True
 
-  if not found:
-    print("Produkt nebyl nalezen")
+    if not found:
+        print("Produkt nebyl nalezen")
 
 
 def total_price():
     if not products:
         print("Sklad je prázdný")
         return
-    total = sum([product['price'] for product in products])
+
+    total = sum([product['price'] * product['amount'] for product in products])
     print(f"Celková cena: {total}$")
 
 
@@ -91,6 +92,7 @@ def max_price():
     if not products:
         print("Sklad je prázdný")
         return
+
     maximum_price = max([product['price'] for product in products])
     for product in products:
         if product['price'] == maximum_price:
@@ -101,6 +103,7 @@ def min_price():
     if not products:
         print("Sklad je prázdný")
         return
+
     minimum_price = min([product['price'] for product in products])
     for product in products:
         if product['price'] == minimum_price:
@@ -111,6 +114,7 @@ def max_amount():
     if not products:
         print("Sklad je prázdný")
         return
+
     maximum_amount = max([product['amount'] for product in products])
     for product in products:
         if product['amount'] == maximum_amount:
@@ -133,8 +137,15 @@ def edit_product():
 
     if 0 <= edit_choice < len(products):
         new_name = input("Zadej nový název produktu: ")
-        new_price = int(input("Zadej novou cenu produktu: "))
-        new_amount = int(input("Zadej nové množství produktu: "))
+        try:
+            new_price = int(input("Zadej novou cenu produktu: "))
+        except ValueError:
+            new_price = 0
+
+        try:
+            new_amount = int(input("Zadej nové množství produktu: "))
+        except ValueError:
+            new_amount = 0
 
         products[edit_choice]['name'] = new_name
         products[edit_choice]['price'] = new_price
@@ -148,74 +159,74 @@ def edit_product():
 
 
 def menu():
-    print("Vítej ve skladu")
-    print("###############\n")
-    print("0. Výpis položek")
-    print("1. Přidání položky")
-    print("2. Odtranění položky")
-    print("3. Celková cena")
-    print("4. Nejvyšší cena")
-    print("5. Nejnižší cena")
-    print("6. Průměrná cena")
-    print("7. Upravit položku")
-    print("8. Hledání položky")
-    print("9. Nejvyšší množství")
+    while True:
+        print("Vítej ve skladu")
+        print("###############\n")
+        print("0. Výpis položek")
+        print("1. Přidání položky")
+        print("2. Odstranění položky")
+        print("3. Celková cena")
+        print("4. Nejvyšší cena")
+        print("5. Nejnižší cena")
+        print("6. Průměrná cena")
+        print("7. Upravit položku")
+        print("8. Hledání položky")
+        print("9. Nejvyšší množství")
+        print("10. Konec")
 
-    choice = int(input("Volba: \n"))
-
-    match choice:
-        case 0:
-            print("### Vypsání položek #### ")
-            print_products()
-            print("")
-            menu()
-        case 1:
-            print("### Přidání položky ###")
-            add_product()
-            print("")
-            menu()
-        case 2:
-            remove_product()
-            print("")
-            menu()
-        case 3:
-            print("### Celková cena ###")
-            total_price()
-            print("")
-            menu()
-        case 4:
-            print("### Nejvyšší cena ###")
-            max_price()
-            print("")
-            menu()
-        case 5:
-            print("### Nejnižší cena ###")
-            min_price()
-            print("")
-            menu()
-        case 6:
-            print("### Průměrná cena ###")
-            average_price()
-            print("")
-            menu()
-        case 7:
-            print("### Upravit položku ###")
-            edit_product()
-            print("")
-            menu()
-        case 8:
-            print("### Hledání položky ###")
-            search_product()
-            print("")
-            menu()
-        case 9:
-            print("### Nejvyšší množství ###")
-            max_amount()
-            print("")
-            menu()
-        case _:
+        try:
+            choice = int(input("Volba: \n"))
+        except ValueError:
             print("Neplatná volba")
-            print("")
-            menu()
+            continue
 
-menu()
+        match choice:
+            case 0:
+                print("### Vypsání položek #### ")
+                print_products()
+                print("")
+            case 1:
+                print("### Přidání položky ###")
+                add_product()
+                print("")
+            case 2:
+                remove_product()
+                print("")
+            case 3:
+                print("### Celková cena ###")
+                total_price()
+                print("")
+            case 4:
+                print("### Nejvyšší cena ###")
+                max_price()
+                print("")
+            case 5:
+                print("### Nejnižší cena ###")
+                min_price()
+                print("")
+            case 6:
+                print("### Průměrná cena ###")
+                average_price()
+                print("")
+            case 7:
+                print("### Upravit položku ###")
+                edit_product()
+                print("")
+            case 8:
+                print("### Hledání položky ###")
+                search_product()
+                print("")
+            case 9:
+                print("### Nejvyšší množství ###")
+                max_amount()
+                print("")
+            case 10:
+                print("Ukončuji program.")
+                break
+            case _:
+                print("Neplatná volba")
+                print("")
+
+
+if __name__ == "__main__":
+    menu()
